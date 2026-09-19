0a3021fb22ee223bc471f4246019b2e9ef51dafc
kaiss55 <kaiss@ymail.com>
2026-09-19 16:22:02 +0800

    Upgrade service worker to version 2
    Updated service worker to version 2 and added cache management.

--- sw.js
+++ sw.js
@@ -1,4 +1,37 @@
-const CACHE_NAME = 'picross-100-v1';
+const CACHE_NAME = 'picross-100-v2';
+const ASSETS = [
+  './',
+  './index.html',
+  './style.css',
+  './app.js',
+  './levels.json',
+  './manifest.json'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) return caches.delete(key);
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
+});const CACHE_NAME = 'picross-100-v1';
 const ASSETS = [
   './',
   './index.html',
